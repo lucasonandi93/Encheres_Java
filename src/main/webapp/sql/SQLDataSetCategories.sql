653aa838218e796868870af54de7eef9dd2abd7e
ldupont2022 <ldupont2022@46SE48-C4PSYJ2.ad.campus-eni.fr>
2023-01-09 14:06:51 +0100

    Add SQL Requests

---
+++ src/main/webapp/sql/SQLDataSetCategories.sql
@@ -0,0 +1,6 @@
+-- dataset for categories
+
+INSERT INTO CATEGORIES (libelle) VALUES 'Informatique';
+INSERT INTO CATEGORIES (libelle) VALUES 'Ameublement';
+INSERT INTO CATEGORIES (libelle) VALUES 'Vêtement';
+INSERT INTO CATEGORIES (libelle) VALUES 'Sport&Loisirs';
