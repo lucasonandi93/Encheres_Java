2f35b081505e879a49dce3ac43aa306586b39baf
Sandra <sandra.lamire.sl@gmail.com>
2023-01-17 14:15:17 +0100

    modif listofauction

--- src/main/java/fr/eni/enchere/servlet/ServletDetailsAuctionPage.java
+++ src/main/java/fr/eni/enchere/servlet/ServletDetailsAuctionPage.java
@@ -23,52 +23,55 @@ import fr.eni.enchere.exceptions.BusinessException;
 @WebServlet("/ServletDetailsAuctionPage")
 public class ServletDetailsAuctionPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @throws ServletException 
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletDetailsAuctionPage() throws ServletException {
-        super();
-    } 
+
+	/**
+	 * @throws ServletException 
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletDetailsAuctionPage() throws ServletException {
+		super();
+	} 
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		HttpSession session = request.getSession();
 		ArticleManager articleManager = new ArticleManager();
 		AuctionManager auctionManager = new AuctionManager();
-		
+
 		try {
 			Article articleOngoing = articleManager.selectById(Integer.parseInt(request.getParameter("articleID")));
 			request.setAttribute("articleOngoing", articleOngoing);
-			
+
 			List<Auction> auctions = auctionManager.selectByNoArticle(Integer.parseInt(request.getParameter("articleID")));
-		if (!auctions.isEmpty()) {
-			Auction auctionOngoing = new Auction();
-			for (Auction auction : auctions) {
-				if(auction.getAuctionAmount()>auctionOngoing.getAuctionAmount()) {
-					auctionOngoing = auction;
+			if (!auctions.isEmpty()) {
+				Auction auctionOngoing = null;
+				for (Auction auction : auctions) {
+					if (auctionOngoing == null)
+						auctionOngoing = auction;
+					else if(auction.getAuctionAmount()>auctionOngoing.getAuctionAmount()) {
+						auctionOngoing = auction;
+					}
 				}
+				System.out.println(auctionOngoing);				
+				request.setAttribute("pseudoBestAuction", auctionOngoing.getUser().getPseudo());
+			}else {
+				request.setAttribute("pseudoBestAuction", articleOngoing.getUser().getPseudo());
 			}
-			request.setAttribute("pseudoBestAuction", auctionOngoing.getUser().getPseudo());
-		}else {
-			request.setAttribute("pseudoBestAuction", articleOngoing.getUser().getPseudo());
-		}
-		
+
 		} catch (NumberFormatException | BusinessException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Session User " + session.getAttribute("user"));
 		System.out.println("Article User " + ((Article)request.getAttribute("articleOngoing")).getUser());
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/detailsAuctionPage.jsp");
 		rd.forward(request, response);
 	}
-	
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
